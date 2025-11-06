495d34a030810378e4cd9c18d2f0f72bb768a096
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-07 00:26:15 +0700

    saya menambahkan strings.xml untuk form formulir

--- app/src/main/java/com/example/praktikum6/view/Home.kt
+++ app/src/main/java/com/example/praktikum6/view/Home.kt
@@ -1,14 +1,8 @@
 package com.example.praktikum6.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +11,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.praktikum6.R
+
 
 
 @Composable
@@ -60,14 +56,11 @@ fun Home(
             modifier = Modifier
                 .width(200.dp)
                 .height(50.dp)
-        )
-
-
-
-
-
-
-
-
+        ){
+            Text(
+                text = stringResource(R.string.button_submit),
+                fontSize = 16.sp
+            )
+        }
     }
 }
