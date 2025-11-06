c61470f875aadeba806ab282f70064c2f9398952
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-07 00:36:02 +0700

    tambahkan composable FormPendaftaranPage untuk input data pengguna

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -1,2 +1,25 @@
 package com.example.praktikum6.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormPendaftaranPage(
+    onback: () -> Unit
+){
+    val context = LocalContext.current
+
+    var namaLengkap by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var statusPerkawinan by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+
+    var namaError by remember { mutableStateOf(false) }
+    var alamatError by remember { mutableStateOf(false) }
+
+    val jenisKelaminOptions = listOf("Laki-laki", "Perempuan")
+    val statusPerkawinanOptions = listOf("Janda", "Lajang", "Duda")
+
+
+
+
+}
