5487f93c75f1edea42079d41b832128e4d99027a
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-07 00:13:29 +0700

    tambahkan logo aplikasi pada tampilan Home

--- app/src/main/java/com/example/praktikum6/view/Home.kt
+++ app/src/main/java/com/example/praktikum6/view/Home.kt
@@ -1,12 +1,15 @@
 package com.example.praktikum6.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -35,6 +38,14 @@ fun Home(
             fontSize = 18.sp,
             modifier = Modifier.padding(bottom = 32.dp)
         )
+        Image(
+            painter = painterResource(id = R.drawable.logo_cardlst), // Ganti sesuai nama file Anda
+            contentDescription = "Logo Aplikasi",
+            modifier = Modifier
+                .size(120.dp)
+                .padding(bottom = 24.dp)
+        )
+
 
 
 
