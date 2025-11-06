158186918d46e3456ac9bbea31e76eaa1df332f7
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-07 00:00:44 +0700

    tambahkan Column dengan alignment center dan padding 16dp

--- app/src/main/java/com/example/praktikum6/view/Home.kt
+++ app/src/main/java/com/example/praktikum6/view/Home.kt
@@ -1,9 +1,24 @@
 package com.example.praktikum6.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun Home(
-    onDaftarClick: () -> Unit,
-)
+    onDaftarClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    }
+}
