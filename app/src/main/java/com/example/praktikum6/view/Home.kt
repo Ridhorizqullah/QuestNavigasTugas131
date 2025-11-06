fff28aa5795637cc4c44f2d8c30e09f990efc97e
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-07 00:08:45 +0700

    tambahkan teks dengan string.welcome_title pada tampilan Home

--- app/src/main/java/com/example/praktikum6/view/Home.kt
+++ app/src/main/java/com/example/praktikum6/view/Home.kt
@@ -3,10 +3,14 @@ package com.example.praktikum6.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -19,6 +23,17 @@ fun Home(
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    )
+    ) {
+        Text(
+            text = stringResource(R.string.welcome_title),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+
+
+
+
+
     }
 }
