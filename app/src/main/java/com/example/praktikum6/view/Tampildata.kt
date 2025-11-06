22752846bb125ec4837cf3e29a5c08319f5ae54a
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-07 01:01:38 +0700

    tambahkan composable Tampildata untuk menampilkan daftar pendaftar

--- app/src/main/java/com/example/praktikum6/view/Tampildata.kt
+++ app/src/main/java/com/example/praktikum6/view/Tampildata.kt
@@ -1,2 +1,11 @@
 package com.example.praktikum6.view
 
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun Tampildata(
+    daftarPendaftar: List<Pendaftar>,
+    onBackToHome: () -> Unit,
+    onGoToForm: () -> Unit
+)
